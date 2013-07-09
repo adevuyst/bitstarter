9298bfdebf9eecd7d52d70155f54cfbd6bdeee4a
Asher De Vuuyst <asherdevuyst@yahoo.com>
2013-07-09 20:51:32 +0000

    Updated to read file

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var responseString =  fs.readFile('index.html'); 
+  response.send(responseString);
 });
 
 var port = process.env.PORT || 5000;
