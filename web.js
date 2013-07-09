1ed3147c28786011be9681eebb5ce7545008a82f
Asher De Vuuyst <asherdevuyst@yahoo.com>
2013-07-09 21:04:31 +0000

    second try

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var responseString =  fs.readFileSync('~/bitstarter/index.html'); 
+    var responseString =  fs.readFileSync('index.html'); 
   response.send(responseString);
 });
 
