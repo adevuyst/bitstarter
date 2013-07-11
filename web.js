91d27995a6c0f9e17587395494de251d9ae0a695
Asher De Vuuyst <asherdevuyst@yahoo.com>
2013-07-11 17:54:53 +0000

    Try 3

--- web.js
+++ web.js
@@ -4,7 +4,25 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var responseString =  fs.readFileSync('index.html'); 
+
+var responseString= ''; 
+
+fs.exists(fileName, function(exists) {
+  if (exists) {
+    fs.stat(fileName, function(error, stats) {
+      fs.open(fileName, "r", function(error, fd) {
+        var buffer = new Buffer(stats.size);
+        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+	    var data = buffer.toString("utf8", 0, buffer.length);
+	    console.log(data);
+            responseString = data;
+            fs.close(fd);
+        });
+      });
+    });
+  }
+});
+ 
   response.send(responseString);
 });
 
